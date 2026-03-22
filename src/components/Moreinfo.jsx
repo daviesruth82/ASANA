ba18a38895ae360db67d47931cf63acadd042cfe
davies ruth <daviesruth82@gmail.com>
2026-03-22 19:48:29 +0100

    completed

--- src/components/Moreinfo.jsx
+++ src/components/Moreinfo.jsx
@@ -1,17 +1,172 @@
-import React from 'react'
-import "./Styles/Moreinfo.css"
+import React from "react";
+import "./Styles/Moreinfo.css";
 
 const Moreinfo = () => {
   return (
-    <div className='more-container'>
-        <div className='more-holder'>
-
+    <div className="more-container">
+      <div className="more-holder">
+        <div className="more-info">
+          <div className="text-header">
+            <h3>
+              The only platform that can support your company at any scale
+            </h3>
+            <ol>
+              <li>
+                Accurate as of December 2023, includes free and paid users.
+              </li>
+            </ol>
+          </div>
+          <div>
+            <button>Get started</button>
+          </div>
         </div>
-      
+        <hr />
+        <div className="info-links">
+          <div className="lists">
+            <img
+              src="https://asana.com/assets/img/brand/asana-logo-favicon.ico"
+              alt="icon"
+            />
+          </div>
+          <div className="lists">
+            <h6>New to Asana?</h6>
+            <ul>
+              <li>Product overview</li>
+              <li>All features</li>
+              <li>Latest feature release</li>
+              <li>Pricing</li>
+              <li>Starter plan</li>
+              <li>Advanced plan</li>
+              <li>Enterprise</li>
+              <li>App integrations</li>
+              <li>AI work management</li>
+              <li>Project management </li>
+              <li>Resource management</li>
+            </ul>
+          </div>
+          <div className="lists">
+            <h6>Use cases</h6>
+            <ul>
+              <li>Campaign management</li>
+              <li>Content calendar</li>
+              <li>Creative production</li>
+              <li>Goal management</li>
+              <li>New hire onboarding</li>
+              <li>Organizational planning</li>
+              <li>Product launches</li>
+              <li>Resource planning</li>
+              <li>Strategic planning</li>
+              <li>Project intake</li>
+              <li>All use cases</li>
+            </ul>
+          </div>
+          <div className="lists">
+            <h6> Solutions</h6>
+
+            <ul>
+              <li>Small business</li>
+              <li>Marketing</li>
+              <li>Operations</li>
+              <li>IT</li>
+              <li>Product</li>
+              <li>Sales</li>
+              <li>Healthcare</li>
+              <li>Retail</li>
+              <li>Government</li>
+              <li>Education</li>
+              <li>Manufacturing</li>
+              <li>Nonprofits</li>
+              <li>Startups</li>
+              <li> All teams</li>
+            </ul>
+          </div>
+          <div className="lists">
+            <h6> Resources</h6>
+
+            <ul>
+              <li>Help Center</li>
+              <li>Get support</li>
+              <li>Asana Academy</li>
+              <li>Certifications</li>
+              <li>Forum</li>
+              <li>Resource center</li>
+              <li>Events and webinars</li>
+              <li>Project templates</li>
+              <li>Customer Success</li>
+              <li>Developers and API</li>
+              <li> Partners </li>
+              <li> Sitemap</li>
+            </ul>
+          </div>
+          <div className="lists">
+            <h6> Company</h6>
+
+            <ul>
+              <li>About us</li>
+              <li>Leadership</li>
+              <li>Customers</li>
+              <li>Careers</li>
+              <li>Inside Asana</li>
+              <li>Culture</li>
+              <li>Press</li>
+              <li>Investor relations</li>
+              <li>Trust and security</li>
+              <li>Privacy</li>
+              <li>Supplier responsibility</li>
+              <li>Sustainability and ESG</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className="foot-con">
+        <cite>© 2026 Asana, Inc.</cite>
+        <div className="globe-con">
+          <img
+            src="https://assets.asana.biz/transform/3ac30492-304b-4644-a39b-e89fad0df303/SD040-web-nav-GlobeIcon-en-US"
+            alt=""
+          />
+          <p>English</p>
+        </div>
+        <div>
+          <img
+            src="data:image/svg+xml,%3Csvg%20width%3D%2216%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20role%3D%22graphics-symbol%22%20aria-labelledby%3D%22x-title-id%22%20aria-hidden%3D%22false%22%20color%3D%22inherit%22%20class%3D%22css-1y9k1o0%20e1bpwjla1%22%3E%3Ctitle%20id%3D%22x-title-id%22%3EX%3C%2Ftitle%3E%3Cpath%20d%3D%22m9.46237%2C6.79045l5.98644%2C-6.81446l-1.4181%2C0l-5.20021%2C5.91569l-4.15031%2C-5.91569l-4.78808%2C0l6.27745%2C8.94642l-6.27745%2C7.14513l1.4181%2C0l5.48802%2C-6.24851l4.38398%2C6.24851l4.78808%2C0m-14.14825%2C-15.04433l2.17859%2C0l10.02903%2C14.04856l-2.17913%2C0%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E"
+            alt=""
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20width%3D%2216%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20role%3D%22graphics-symbol%22%20aria-labelledby%3D%22linkedin-title-id%22%20aria-hidden%3D%22false%22%20color%3D%22inherit%22%20class%3D%22css-1y9k1o0%20e1bpwjla1%22%3E%3Ctitle%20id%3D%22linkedin-title-id%22%3ELinkedin%3C%2Ftitle%3E%3Cpath%20d%3D%22M4.02331%2013.0463H1.25405V4.18301H4.02331V13.0463ZM2.56835%203.07474H2.54846C1.54675%203.07474%200.895996%202.39699%200.895996%201.53879C0.895996%200.663541%201.56522%200%202.58824%200C3.60984%200%204.23786%200.662121%204.25775%201.53595C4.25775%202.39557%203.60842%203.07474%202.56835%203.07474ZM15.1046%2013.0463H11.9645V8.4598C11.9645%207.25918%2011.4743%206.44076%2010.393%206.44076C9.56608%206.44076%209.10714%206.99347%208.89402%207.5263C8.81445%207.71669%208.82582%207.98381%208.82582%208.25093V13.0463H5.71413C5.71413%2013.0463%205.75392%204.92186%205.71413%204.18301H8.82582V5.57403C9.00911%204.96591%2010.0037%204.09776%2011.5908%204.09776C13.5587%204.09776%2015.1046%205.37227%2015.1046%208.11737V13.0463Z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E"
+            alt=""
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20width%3D%2216%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20role%3D%22graphics-symbol%22%20aria-labelledby%3D%22instagram-title-id%22%20aria-hidden%3D%22false%22%20color%3D%22inherit%22%20class%3D%22css-1y9k1o0%20e1bpwjla1%22%3E%3Ctitle%20id%3D%22instagram-title-id%22%3EInstagram%3C%2Ftitle%3E%3Cpath%20d%3D%22M8%202.26175C9.869%202.26175%2010.0907%202.26875%2010.8292%202.30258C12.7262%202.38892%2013.6122%203.289%2013.6986%205.172C13.7324%205.90992%2013.7388%206.13158%2013.7388%208.00058C13.7388%209.87017%2013.7318%2010.0912%2013.6986%2010.8292C13.6117%2012.7104%2012.7279%2013.6122%2010.8292%2013.6986C10.0907%2013.7324%209.87017%2013.7394%208%2013.7394C6.131%2013.7394%205.90933%2013.7324%205.17142%2013.6986C3.26975%2013.6117%202.38833%2012.7075%202.302%2010.8286C2.26817%2010.0907%202.26117%209.86958%202.26117%208C2.26117%206.131%202.26875%205.90992%202.302%205.17142C2.38892%203.289%203.27267%202.38833%205.17142%202.302C5.90992%202.26875%206.131%202.26175%208%202.26175ZM8%201C6.09892%201%205.86092%201.00817%205.11425%201.042C2.57208%201.15867%201.15925%202.56917%201.04258%205.11367C1.00817%205.86092%201%206.09892%201%208C1%209.90108%201.00817%2010.1397%201.042%2010.8863C1.15867%2013.4285%202.56917%2014.8413%205.11367%2014.958C5.86092%2014.9918%206.09892%2015%208%2015C9.90108%2015%2010.1397%2014.9918%2010.8863%2014.958C13.4262%2014.8413%2014.8425%2013.4308%2014.9574%2010.8863C14.9918%2010.1397%2015%209.90108%2015%208C15%206.09892%2014.9918%205.86092%2014.958%205.11425C14.8437%202.57442%2013.4314%201.15925%2010.8869%201.04258C10.1397%201.00817%209.90108%201%208%201V1ZM8%204.4055C6.01492%204.4055%204.4055%206.01492%204.4055%208C4.4055%209.98508%206.01492%2011.5951%208%2011.5951C9.98508%2011.5951%2011.5945%209.98567%2011.5945%208C11.5945%206.01492%209.98508%204.4055%208%204.4055ZM8%2010.3333C6.71142%2010.3333%205.66667%209.28917%205.66667%208C5.66667%206.71142%206.71142%205.66667%208%205.66667C9.28858%205.66667%2010.3333%206.71142%2010.3333%208C10.3333%209.28917%209.28858%2010.3333%208%2010.3333ZM11.7368%203.42375C11.2725%203.42375%2010.8962%203.8%2010.8962%204.26375C10.8962%204.7275%2011.2725%205.10375%2011.7368%205.10375C12.2006%205.10375%2012.5762%204.7275%2012.5762%204.26375C12.5762%203.8%2012.2006%203.42375%2011.7368%203.42375Z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E"
+            alt=""
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20width%3D%2216%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20role%3D%22graphics-symbol%22%20aria-labelledby%3D%22facebook-title-id%22%20aria-hidden%3D%22false%22%20color%3D%22inherit%22%20class%3D%22css-1y9k1o0%20e1bpwjla1%22%3E%3Ctitle%20id%3D%22facebook-title-id%22%3EFacebook%3C%2Ftitle%3E%3Cpath%20d%3D%22M10.9218%208.53781L11.2916%206.12389H8.9763V4.55766C8.9763%203.89782%209.29917%203.25325%2010.3371%203.25325H11.3902V1.1986C11.3902%201.1986%2010.4345%201.0354%209.52108%201.0354C7.61436%201.0354%206.36747%202.1907%206.36747%204.2841V6.12389H4.24707V8.53781H6.36747V14.3718H8.9763V8.53781H10.9218Z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E"
+            alt=""
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20width%3D%2216%22%20height%3D%2216%22%20viewBox%3D%220%200%2016%2016%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20role%3D%22graphics-symbol%22%20aria-labelledby%3D%22youtube-title-id%22%20aria-hidden%3D%22false%22%20color%3D%22inherit%22%20class%3D%22css-1y9k1o0%20e1bpwjla1%22%3E%3Ctitle%20id%3D%22youtube-title-id%22%3EYoutube%3C%2Ftitle%3E%3Cpath%20d%3D%22M7.99504%202.40137C7.99504%202.40137%202.99439%202.40137%201.73929%202.7275C1.06727%202.91527%200.513907%203.46864%200.326136%204.15054C7.4248e-06%205.40564%200%208.00485%200%208.00485C0%208.00485%207.4248e-06%2010.6139%200.326136%2011.8493C0.513907%2012.5312%201.05738%2013.0746%201.73929%2013.2624C3.00427%2013.5984%207.99504%2013.5984%207.99504%2013.5984C7.99504%2013.5984%2013.0056%2013.5984%2014.2607%2013.2723C14.9426%2013.0845%2015.4861%2012.5509%2015.6639%2011.8592C16%2010.6139%2016%208.01474%2016%208.01474C16%208.01474%2016.0098%205.40564%2015.6639%204.15054C15.4861%203.46864%2014.9426%202.92517%2014.2607%202.74728C13.0056%202.40138%207.99504%202.40137%207.99504%202.40137ZM6.40391%205.6034L10.5646%208.00485L6.40391%2010.3964V5.6034Z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E"
+            alt=""
+          />
+        </div>
+        <div>
+          <p>Terms and privacy</p>
+        </div>
+        <div className="foot-img">
+          <img
+            src="https://assets.asana.biz/m/789462af5a590563/original/Download_App_Store_Badge_US.svg"
+            alt=""
+          />
+          <img
+            src="https://assets.asana.biz/m/2830b6f5895bc51c/original/Google_Play_EN.svg"
+            alt=""
+          />
+        </div>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default Moreinfo
+export default Moreinfo;
